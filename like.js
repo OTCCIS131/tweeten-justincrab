dc29c17c2cd446fb0d6d86cd76fe4e99563a4c89
Unknown <ajbeers89@gmail.com>
2017-09-27 13:19:29 -0500

    attempt

---
+++ like.js
@@ -0,0 +1,24 @@
+// let buttons = document.getElementsByClassName("glyphicon glyphicon-heart")
+
+
+
+// btn.array.forEach(function(element) {
+//     button.addEventListender(click, e);{
+//         let currentCount = btn.innerHTML
+//         let currentCount = btn.nextSibling.textContent 
+//          new count = parseInt(currentCount) +1
+
+
+//     }
+// }, this);
+
+function likeButton(){
+  const existingElements = Array.from(document.getElementsByClassName("glyphicon glyphicon-heart");
+
+   
+}
+
+
+function alertFunction(){
+    alert("Please work!")
+}
