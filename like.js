0987dcc0b7016f7c5075741ed3ee75a60347d074
Unknown <ajbeers89@gmail.com>
2017-10-02 12:20:50 -0500

    button adds numbers ~ not correctly though

--- like.js
+++ like.js
@@ -12,13 +12,21 @@
 //     }
 // }, this);
 
-function likeButton(){
-  const existingElements = Array.from(document.getElementsByClassName("glyphicon glyphicon-heart");
 
-   
-}
+// Web page loading event
+// window.addEventListener("load", e => {
+//   alert("The page has been loaded!");
+// });
 
 
-function alertFunction(){
-    alert("Please work!")
-}
+  let count = 0;
+
+ let buttons =  Array.from(document.getElementsByClassName("glyphicon glyphicon-heart"))
+
+ buttons.forEach(btn =>{
+  btn.addEventListener("click", () =>{
+    count++;
+    btn.textContent = count
+    // alert("Please work!");
+  });
+ });
