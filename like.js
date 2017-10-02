d98e23510be497a3c9cdc512aacec94d102777ad
Unknown <ajbeers89@gmail.com>
2017-10-02 13:06:10 -0500

    deleted my unused code

--- like.js
+++ like.js
@@ -1,25 +1,3 @@
-// let buttons = document.getElementsByClassName("glyphicon glyphicon-heart")
-
-
-
-// btn.array.forEach(function(element) {
-//     button.addEventListender(click, e);{
-//         let currentCount = btn.innerHTML
-//         let currentCount = btn.nextSibling.textContent 
-//          new count = parseInt(currentCount) +1
-
-
-//     }
-// }, this);
-
-
-// Web page loading event
-// window.addEventListener("load", e => {
-//   alert("The page has been loaded!");
-// });
-
-
-
  let buttons =  Array.from(document.getElementsByClassName("glyphicon glyphicon-heart"))
 
  buttons.forEach(btn =>{
