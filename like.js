95b316da5b6a2fc08390462b6a5cf8050550fbe0
Justin Crabtree <justincrab@gmail.com>
2017-10-04 13:22:09 -0500

    trying to change to jquery

--- like.js
+++ like.js
@@ -1,8 +1,16 @@
- let buttons =  Array.from(document.getElementsByClassName("glyphicon glyphicon-heart"))
+// let buttons =  Array.from(document.getElementsByClassName("glyphicon glyphicon-heart"))
 
- buttons.forEach(btn =>{
-  btn.addEventListener("click", () =>{
-    var count = parseInt(btn.textContent) + 1
-    btn.textContent = '\xA0' + count.toString() +'\xA0'
-  });
- });
+// buttons.forEach(btn =>{
+//  btn.addEventListener("click", () =>{
+//    var count = parseInt(btn.textContent) + 1
+//    btn.textContent = '\xA0' + count.toString() +'\xA0'
+//  });
+// });
+$(function() {
+  $('.glyphicon.glyphicon-heart').click(({target}) => { //((({target}))) sets "this" to what is clicked
+    let countTag = $($(target).children('span')[0])
+    // debugger
+    let count = parseInt(countTag.text())+1
+    $(target).Text(count)
+  })
+})
