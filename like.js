91f54bd6398a2b16089b5b120b9d96345750d265
Justin Crabtree <justincrab@gmail.com>
2017-10-15 23:02:18 -0500

    attempting New Tweet in JQUERY
    i am not a fan of jquery

--- like.js
+++ like.js
@@ -1,12 +1,3 @@
-// let buttons =  Array.from(document.getElementsByClassName("glyphicon glyphicon-heart"))
-
-// buttons.forEach(btn =>{
-//  btn.addEventListener("click", () =>{
-//    var count = parseInt(btn.textContent) + 1
-//    btn.textContent = '\xA0' + count.toString() +'\xA0'
-//  });
-// });
-
 //new tweet - VANILLA
 document.getElementById("tweetbutton").addEventListener("click", () => {
   // Get the full tweet package to clone
@@ -23,14 +14,35 @@ document.getElementById("tweetbutton").addEventListener("click", () => {
   document.getElementById("middleLower").insertBefore(copy, document.getElementById("tweetStats").nextSibling);
 })
 
+// //new tweet - JQUERY
+// $(".tweetbutton").on("click", () => {
+//   // Get the full tweet package to clone
+//   var thetweet = $("#thetweet");
 
+//   // Copy the tweet and the text in the text area
+//   var copy = thetweet.cloneNode(true);
+//   var copytext = $("#TweetForm").value;
 
-//like button counter
-$(function() {
-  $('.glyphicon.glyphicon-heart').click(({target}) => { //((({target}))) sets "this" to what is clicked
-    let countTag = $($(target).children('span')[0])
-    // debugger
-    let count = parseInt(countTag.text())+1
-    $(target).Text(count)
-  })
+//   // add the text to the new tweet
+//   copy.querySelector("#textofTweet").innerHTML = copytext;
+
+//   // append the new tweet to the top of the list (but after the tweet stats bar)
+//   $("#middleLower").insertBefore(copy, $("#tweetStats").nextSibling);
+// })
+
+//like button counter - JQUERY
+$( ".heart" ).on("click", function() {
+  var count = parseInt(document.getElementById("heartcount").textContent)
+  count += 1;
+  $(".heartcount").text('\xA0' + count + '\xA0')
 })
+
+//like button counter - VANILLA
+// let buttons =  Array.from(document.getElementsByClassName("glyphicon glyphicon-heart"))
+
+// buttons.forEach(btn =>{
+//  btn.addEventListener("click", () =>{
+//    var count = parseInt(btn.textContent) + 1
+//    btn.textContent = '\xA0' + count.toString() +'\xA0'
+//  });
+// });
